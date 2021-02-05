2fe58728f4926c779af2f3a7386440742cf74a5c
liupang <>
2021-02-05 18:13:36 +0800

    添加 activity 共享

--- app/src/main/java/com/live/simple2/animation/shareElement/StartActivity.java
+++ app/src/main/java/com/live/simple2/animation/shareElement/StartActivity.java
@@ -5,37 +5,104 @@ import android.os.Bundle;
 import android.transition.Slide;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.util.Pair;
 import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.live.simple2.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StartActivity extends AppCompatActivity {
     private ImageView startImg;
+    private RecyclerView recyclerView;
+
+    private ArrayList<String> pics = new ArrayList<>();
+
+    private GridLayoutManager layoutManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_shareelement_start);
         startImg = findViewById(R.id.startImg);
+        recyclerView = findViewById(R.id.recyclerView);
 
-        ViewCompat.setTransitionName(startImg,"stratImg");
+        pics.add("https://cdn.sspai.com/article/365d4624-883e-ceea-f475-f6e06e0d7a88.png?imageMogr2/auto-orient/quality/95/thumbnail/!1420x708r/gravity/Center/crop/1420x708/interlace/1");
+        pics.add("https://cdn.sspai.com/2021/02/04/f68ab27c9955a0ff8cf1e91069a09800.png?imageView2/2/w/1120/q/90/interlace/1/ignore-error/1");
+        pics.add("https://cdn.sspai.com/2021/02/04/b49da3a37d05aa605ef9334339186ffa.png?imageView2/2/w/1120/q/90/interlace/1/ignore-error/1");
+        pics.add("https://cdn.sspai.com/2021/02/03/bb8740027c884869eed801bfea970e78.jpeg?imageView2/2/w/1120/q/90/interlace/1/ignore-error/1");
 
-        startImg.setOnClickListener( view -> {
-            Intent intent = new Intent(this,TargetActivity.class);
-            Pair<View,String> pair1 = new Pair<>(startImg,ViewCompat.getTransitionName(startImg));
+
+        startImg.setOnClickListener(view -> {
+            ViewCompat.setTransitionName(startImg, "stratImg");
+
+            Intent intent = new Intent(this, TargetActivity.class);
+            Pair<View, String> pair1 = new Pair<>(startImg, ViewCompat.getTransitionName(startImg));
             /**
              *4、生成带有共享元素的Bundle，这样系统才会知道这几个元素需要做动画
              */
             ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this, pair1);
-            ActivityCompat.startActivity(this,intent,activityOptionsCompat.toBundle());
+            ActivityCompat.startActivity(this, intent, activityOptionsCompat.toBundle());
         });
 
+        layoutManager = new GridLayoutManager(this, 2);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new RecyclerView.Adapter<VH>() {
+            @NonNull
+            @Override
+            public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                VH vh = new VH(getLayoutInflater().inflate(R.layout.activity_animation_shareelement_start_item, null));
+                return vh;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull VH holder, int position) {
+                Glide.with(holder.itemView.getContext()).load(pics.get(position)).into(holder.imageView);
+                holder.imageView.setOnClickListener(view -> {
+
+                    TargetActivity.open(StartActivity.this, pics.get(position),pics, holder.imageView,
+                            recyclerView.getChildAt(layoutManager.findFirstVisibleItemPosition()), position);
+//                    ViewCompat.setTransitionName(holder.imageView, "stratImg");
+//                    Intent intent = new Intent(StartActivity.this, TargetActivity.class);
+//                    Pair<View, String> pair1 = new Pair<>(holder.imageView, ViewCompat.getTransitionName(holder.imageView));
+//                    /**
+//                     *4、生成带有共享元素的Bundle，这样系统才会知道这几个元素需要做动画
+//                     */
+//                    ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(StartActivity.this, pair1);
+//
+//                    Bundle bundle = activityOptionsCompat.toBundle();
+//                    intent.putExtra("pic",pics.get(position));
+//
+//                    StartActivity.this.startActivity(intent,bundle);
+                });
+            }
+
+            @Override
+            public int getItemCount() {
+                return pics.size();
+            }
+        });
+    }
+
+    private static class VH extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+
+        public VH(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.img);
+        }
     }
 }
--- app/src/main/java/com/live/simple2/animation/shareElement/TargetActivity.java
+++ app/src/main/java/com/live/simple2/animation/shareElement/TargetActivity.java
@@ -1,39 +1,247 @@
 package com.live.simple2.animation.shareElement;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.transition.ChangeBounds;
 import android.transition.ChangeTransform;
 import android.transition.Fade;
-import android.transition.Transition;
 import android.transition.TransitionSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.app.SharedElementCallback;
+import androidx.core.util.Pair;
 import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.bumptech.glide.Glide;
 import com.live.simple2.R;
+import com.live.simple2.utils.StringUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class TargetActivity extends AppCompatActivity {
     private ImageView img;
 
+    private ViewPager2 viewPager;
+
+    private List<String> pics;
+
+    private String pic;
+    private int selectPosition = 0;
+
+    private static final String SHARE_NAME = "share_name";
+    private static final String SHARE_PIC_KEY = "share_pic_key";
+    private static final String SHARE_PIC_KEY_FOR_LIST = "share_pic_list_key";
+    private static final String SHARE_PIC_KEY_FOR_SELECT= "share_pic_select_key";
+
+    public static void open(Activity src, String currentPic, ArrayList<String> pics, ImageView srcImg){
+        ViewCompat.setTransitionName(srcImg, SHARE_NAME);
+        Intent intent = new Intent(src, TargetActivity.class);
+        Pair<View, String> pair1 = new Pair<>(srcImg, ViewCompat.getTransitionName(srcImg));
+        ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(src, pair1);
+
+        Bundle bundle = activityOptionsCompat.toBundle();
+        intent.putExtra(SHARE_PIC_KEY,currentPic);
+        intent.putStringArrayListExtra(SHARE_PIC_KEY_FOR_LIST,pics);
+        src.startActivity(intent,bundle);
+    }
+    public static void open(Activity src, String currentPic, ArrayList<String> pics, ImageView srcImg, View fristView, int postition){
+        ViewCompat.setTransitionName(srcImg, SHARE_NAME);
+        ViewCompat.setTransitionName(fristView, SHARE_NAME+1);
+        Intent intent = new Intent(src, TargetActivity.class);
+        Pair<View, String> pair1 = new Pair<>(srcImg, ViewCompat.getTransitionName(srcImg));
+        Pair<View, String> pair2 = new Pair<>(fristView, ViewCompat.getTransitionName(fristView));
+        ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(src, pair1, pair2);
+
+        Bundle bundle = activityOptionsCompat.toBundle();
+        intent.putExtra(SHARE_PIC_KEY, currentPic);
+        intent.putExtra(SHARE_PIC_KEY_FOR_SELECT, postition);
+        intent.putStringArrayListExtra(SHARE_PIC_KEY_FOR_LIST, pics);
+        src.startActivity(intent,bundle);
+    }
+//    public static void open(Activity src, String currentPic, List<View> currentVisiableViews, ArrayList<String> pics, ImageView srcImg){
+//        for (View view : currentVisiableViews){
+//            ViewCompat.setTransitionName(srcImg, SHARE_NAME);
+//        }
+//
+//        Intent intent = new Intent(src, TargetActivity.class);
+//        Pair<View, String> pair1 = new Pair<>(srcImg, ViewCompat.getTransitionName(srcImg));
+//        ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(src, pair1);
+//
+//        Bundle bundle = activityOptionsCompat.toBundle();
+//        intent.putExtra(SHARE_PIC_KEY,currentPic);
+//        intent.putStringArrayListExtra(SHARE_PIC_KEY_FOR_LIST,pics);
+//        src.startActivity(intent,bundle);
+//    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setEnterSharedElementCallback(new SharedElementCallback() {
+            @Override
+            public void onSharedElementStart(List<String> sharedElementNames, List<View> sharedElements, List<View> sharedElementSnapshots) {
+                super.onSharedElementStart(sharedElementNames, sharedElements, sharedElementSnapshots);
+                Log.e("sun","onSharedElementStart");
+            }
+
+            @Override
+            public void onSharedElementEnd(List<String> sharedElementNames, List<View> sharedElements, List<View> sharedElementSnapshots) {
+                super.onSharedElementEnd(sharedElementNames, sharedElements, sharedElementSnapshots);
+                Log.e("sun","onSharedElementEnd");
+            }
+
+            @Override
+            public void onRejectSharedElements(List<View> rejectedSharedElements) {
+                super.onRejectSharedElements(rejectedSharedElements);
+                Log.e("sun","onRejectSharedElements");
+            }
+
+            @Override
+            public void onMapSharedElements(List<String> names, Map<String, View> sharedElements) {
+                super.onMapSharedElements(names, sharedElements);
+                Log.e("sun","onMapSharedElements");
+            }
+
+            @Override
+            public Parcelable onCaptureSharedElementSnapshot(View sharedElement, Matrix viewToGlobalMatrix, RectF screenBounds) {
+                Log.e("sun","onCaptureSharedElementSnapshot");
+                return super.onCaptureSharedElementSnapshot(sharedElement, viewToGlobalMatrix, screenBounds);
+            }
+
+            @Override
+            public View onCreateSnapshotView(Context context, Parcelable snapshot) {
+                Log.e("sun","onCreateSnapshotView");
+                return super.onCreateSnapshotView(context, snapshot);
+            }
+
+            @Override
+            public void onSharedElementsArrived(List<String> sharedElementNames, List<View> sharedElements, OnSharedElementsReadyListener listener) {
+                Log.e("sun","onSharedElementsArrived");
+                super.onSharedElementsArrived(sharedElementNames, sharedElements, listener);
+            }
+        });
+        setExitSharedElementCallback(new SharedElementCallback() {
+            @Override
+            public void onSharedElementStart(List<String> sharedElementNames, List<View> sharedElements, List<View> sharedElementSnapshots) {
+                super.onSharedElementStart(sharedElementNames, sharedElements, sharedElementSnapshots);
+                Log.e("sun","onSharedElementStart");
+            }
+
+            @Override
+            public void onSharedElementEnd(List<String> sharedElementNames, List<View> sharedElements, List<View> sharedElementSnapshots) {
+                super.onSharedElementEnd(sharedElementNames, sharedElements, sharedElementSnapshots);
+                Log.e("sun","onSharedElementEnd");
+            }
+
+            @Override
+            public void onRejectSharedElements(List<View> rejectedSharedElements) {
+                super.onRejectSharedElements(rejectedSharedElements);
+                Log.e("sun","onRejectSharedElements");
+            }
+
+            @Override
+            public void onMapSharedElements(List<String> names, Map<String, View> sharedElements) {
+                super.onMapSharedElements(names, sharedElements);
+                Log.e("sun","onMapSharedElements");
+            }
+
+            @Override
+            public Parcelable onCaptureSharedElementSnapshot(View sharedElement, Matrix viewToGlobalMatrix, RectF screenBounds) {
+
+                Log.e("sun","onCaptureSharedElementSnapshot");
+                return super.onCaptureSharedElementSnapshot(sharedElement, viewToGlobalMatrix, screenBounds);
+            }
+
+            @Override
+            public View onCreateSnapshotView(Context context, Parcelable snapshot) {
+                Log.e("sun","onCreateSnapshotView");
+                return super.onCreateSnapshotView(context, snapshot);
+            }
+
+            @Override
+            public void onSharedElementsArrived(List<String> sharedElementNames, List<View> sharedElements, OnSharedElementsReadyListener listener) {
+                super.onSharedElementsArrived(sharedElementNames, sharedElements, listener);
+                Log.e("sun","onSharedElementsArrived");
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_shareelement_target);
         img = findViewById(R.id.targetImg);
+        viewPager = findViewById(R.id.viewPager);
 
-        ViewCompat.setTransitionName(img,"stratImg");
+        pic = getIntent().getStringExtra(SHARE_PIC_KEY);
+
+        pics = getIntent().getStringArrayListExtra(SHARE_PIC_KEY_FOR_LIST);
+
+        selectPosition = getIntent().getIntExtra(SHARE_PIC_KEY_FOR_SELECT, 0);
 
         getWindow().setEnterTransition(new Fade());
         getWindow().setExitTransition(new Fade());
 
+        if (!StringUtil.isEmpty(pic)){
+            Glide.with(this).load(pic).into(img);
 
-        TransitionSet transitionSet = new TransitionSet();
-        transitionSet.addTransition(new ChangeBounds());
-        transitionSet.addTransition(new ChangeTransform());
-        transitionSet.addTarget(img);
-        getWindow().setSharedElementEnterTransition(transitionSet);
-        getWindow().setSharedElementExitTransition(transitionSet);
+            ViewCompat.setTransitionName(img,SHARE_NAME);
+
+            TransitionSet transitionSet = new TransitionSet();
+            transitionSet.addTransition(new ChangeBounds());
+            transitionSet.addTransition(new ChangeTransform());
+            transitionSet.addTarget(img);
+            getWindow().setSharedElementEnterTransition(transitionSet);
+        }
+
+        viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        viewPager.setAdapter(new RecyclerView.Adapter<VH>() {
+            @NonNull
+            @Override
+            public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                VH vh = new VH(getLayoutInflater().inflate(R.layout.activity_animation_shareelement_target_item, parent, false));
+                return vh;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull VH holder, int position) {
+                Glide.with(holder.itemView.getContext()).load(pics.get(position)).into(holder.imageView);
+                holder.imageView.setOnClickListener( view -> {
+                    Toast.makeText(view.getContext(), "哈", Toast.LENGTH_SHORT).show();
+                    ViewCompat.setTransitionName(view,SHARE_NAME+1);
+                    TransitionSet transitionSet = new TransitionSet();
+                    transitionSet.addTransition(new ChangeBounds());
+                    transitionSet.addTransition(new ChangeTransform());
+                    transitionSet.addTarget(view);
+                    getWindow().setSharedElementExitTransition(transitionSet);
+                });
+            }
+
+            @Override
+            public int getItemCount() {
+                return pics == null ? 0 : pics.size();
+            }
+        });
+    }
+
+    private static class VH extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+
+        public VH(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.img);
+        }
     }
 }
